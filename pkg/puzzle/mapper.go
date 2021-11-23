62faef1634c4c77adab1ed71182ce8f0d0b0e2ed
Tim de Klijn <timdeklijn@gmail.com>
2021-11-23 07:15:21 +0100

    convert year, day and part to key

--- pkg/puzzle/mapper.go
+++ pkg/puzzle/mapper.go
@@ -1,6 +1,9 @@
 package puzzle
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/timdeklijn/aoc/pkg/solution"
 	"github.com/timdeklijn/aoc/pkg/y2020"
 )
@@ -9,8 +12,19 @@ var solutionMap = map[string]solution.Solution{
 	"20200101": &y2020.P0101{},
 }
 
-// TODO: add function to convert y,d,p to YYYYDDPP string
+func toKey(y, d, p int) string {
+	yS := strconv.Itoa(y)
+	var dS string
+	if d < 10 {
+		dS = fmt.Sprintf("%02d", d)
+	}
+	dS = strconv.Itoa(d)
+	pS := fmt.Sprintf("%02d", p)
+	return yS + dS + pS
+}
+
 func Map(y, d, p int) solution.Solution {
 	// TODO: add return when solution is not (yet) implemented
-	return solutionMap["20200101"]
+	key := toKey(y, d, p)
+	return solutionMap[key]
 }
